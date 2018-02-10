a9aedbe6b0d5812a74e170b9b4e81f3791281fe2
eric nguyen <eric.tung.nguyen@gmail.com>
2018-02-09 23:15:22 -0500

    updated the button to snap a photo

--- pictureServer.js
+++ pictureServer.js
@@ -76,6 +76,27 @@ var Webcam = NodeWebcam.create( opts ); //starting up the webcam
 
 
 
+// ---------------- MY OWN TAKE PICTURE FUNCTION ON BUTTON CLICK ------------
+
+function takeApic() {
+    /// First, we create a name for the new picture.
+    /// The .replace() function removes all special characters from the date.
+    /// This way we can use it as the filename.
+    var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+
+    console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
+
+    //Third, the picture is  taken and saved to the `public/`` folder
+    NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
+    io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
+    /// The browser will take this new name and load the picture from the public folder.
+  });
+
+  });
+
+// ---------------------------------------------------------------------------
+
+
 //---------------------- SERIAL COMMUNICATION (Arduino) ----------------------//
 // start the serial port connection and read on newlines
 const serial = new SerialPort(process.argv[2], {});
@@ -88,6 +109,11 @@ serial.pipe(parser);
 parser.on('data', function(data) {
   console.log('Data:', data);
   io.emit('server-msg', data);
+    
+  //  Call takeApic(), my own function on click
+    if(data == "light") {
+        takeApic();
+    } 
 });
 //----------------------------------------------------------------------------//
 
